94b3a370fc340ff60c6eed8c8c65e20739a64cda
Sinyushkin Timofey <y.timko56@gmail.com>
2024-07-25 17:57:28 +0800

    [backend] move get_elasticsearch_query to external service function add elasticsearch index creation before app running

---
+++ backend/androxine/exercise/services.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from elasticsearch_dsl import Q
+
+
+def get_exercise_elasticsearch_query(name: Optional[str], category: Optional[str]):
+    matches = []
+    if category:
+        matches.append({"match": {"category": category}})
+    if name:
+        matches.append({"match": {"name": name}})
+
+    if matches:
+        elasticsearch_query = Q({
+            "bool": {
+                "should": matches
+            }
+        })
+    else:
+        elasticsearch_query = Q({
+            "match_all": {},
+        })
+
+    return elasticsearch_query
--- backend/androxine/exercise/views.py
+++ backend/androxine/exercise/views.py
@@ -1,48 +1,26 @@
 from rest_framework.generics import ListAPIView
 
-from elasticsearch_dsl import Q
-
 from exercise.models import ExerciseCategory
 from exercise.documents import ExerciseDocument
 from exercise.serializers import ExerciseCategorySerializer, ExerciseSerializer
+from exercise.services import get_exercise_elasticsearch_query
 
 
 class ExerciseCategoryListView(ListAPIView):
     queryset = ExerciseCategory.objects.all()
     serializer_class = ExerciseCategorySerializer
 
-    def get_queryset(self):
-        return super().get_queryset()
-
 
 class ExerciseListView(ListAPIView):
     serializer_class = ExerciseSerializer
 
-    def get_elasticsearch_query(self):
+    def get_queryset(self):
         category = self.request.query_params.get('category')
         name = self.request.query_params.get('name')
 
-        matches = []
-        if category:
-            matches.append({"match": {"category": category}})
-        if name:
-            matches.append({"match": {"name": name}})
-
-        if matches:
-            elasticsearch_query = Q({
-                "bool": {
-                    "should": matches
-                }
-            })
-        else:
-            elasticsearch_query = Q({
-                "match_all": {},
-            })
-
-        return elasticsearch_query
-
-    def get_queryset(self):
-        search = ExerciseDocument.search().query(self.get_elasticsearch_query())
+        search = ExerciseDocument.search().query(
+            get_exercise_elasticsearch_query(name, category)
+        )
         response = search.execute()
 
         return response
