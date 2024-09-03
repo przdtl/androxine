c1f4b11a50d8a1363c431bb53e7bfef832fbb45e
Sinyushkin Timofey <y.timko56@gmail.com>
2024-09-03 15:23:31 +0700

    [frontend] init frontend app create dev and prod docker images update .gitignore for react design base pages change backend tests running only if backend folder updates

---
+++ frontend/androxine/src/App.js
@@ -0,0 +1,10 @@
+import { Routers } from './Routes';
+import './App.css';
+
+function App() {
+  return (
+    <Routers />
+  );
+}
+
+export default App;
---
+++ frontend/androxine/src/Routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export const Routers = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/'>
+
+                </Route>
+            </Routes>
+        </Router>
+    )
+}
+
